ffd333796e3875072c2ff4fde2c07bb426009bcd
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-17 09:51:37 +1000

    Add contact form, city name and email

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,5 +6,15 @@ export default () => (
     <Menu />
     <h3>CONTACT</h3>
     <p>If you have any questions, please don't hesitate to contact using form below...</p>
+    <form>
+      <input placeholder="Name"></input>
+      <input placeholder="Email"></input>
+      <input placeholder="Message"></input>
+      <button type="submit">Send</button>
+    </form>
+    <div>
+      <div>Sydney, NSW</div>
+      <div>monicamendesmontanha@gmail.com</div>
+    </div>
   </>
 )
