645a0b6529614e60ee149fe4ba0a4c306196b725
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-17 09:51:05 +1000

    Change name

--- src/pages/components/Menu.js
+++ src/pages/components/Menu.js
@@ -4,10 +4,9 @@ import { Link } from "gatsby"
 export default () => (
 
 <>
-This is a menu.
 <Link to="/">Home</Link>
 <Link to="/about/">About</Link>
-<Link to="/skills/">Projects</Link>
+<Link to="/skills/">Skills</Link>
 <Link to="/projects/">Projects</Link>
 <Link to="/contact/">Contact</Link>
 </>
