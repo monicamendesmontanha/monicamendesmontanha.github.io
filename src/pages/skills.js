02c12c07a80e9bb1b6f5f095c0e0aa70f681780b
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-17 10:15:13 +1000

    format page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -5,11 +5,11 @@ export default () => (
   <>
     <Menu />
     <h3>SKILLS</h3>
-    <p> Javascript     React    Ruby    Ruby on Rails   Express
-
-Node.js    JQuery    Graphql   HTML 5    CSS 3    SASS</p>
+    <p>
+      Javascript     React    Ruby    Ruby on Rails   Express
+      Node.js    JQuery    Graphql   HTML 5    CSS 3    SASS
+    </p>
     <h3>INTERESTS</h3>
     <p>💜 Accessibility   🗓  Planning   🔋Productivity   💡 Mentoring</p>
-
   </>
 )
