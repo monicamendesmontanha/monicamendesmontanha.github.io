5a8c7cc6eee4c9e2082ba82b2639e5df490c8cec
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-17 10:15:30 +1000

    Add links to twitter, likedin and github

--- src/pages/components/Menu.js
+++ src/pages/components/Menu.js
@@ -2,13 +2,45 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default () => (
-
-<>
-<Link to="/">Home</Link>
-<Link to="/about/">About</Link>
-<Link to="/skills/">Skills</Link>
-<Link to="/projects/">Projects</Link>
-<Link to="/contact/">Contact</Link>
-</>
-
+  <>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about/">About</Link>
+      <Link to="/skills/">Skills</Link>
+      <Link to="/projects/">Projects</Link>
+      <Link to="/contact/">Contact</Link>
+    </div>
+    <div>
+    <a
+        href="https://"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        CV
+      </a>
+    </div>
+    <div>
+      <a
+        href="https://github.com/monicamendesmontanha"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        GitHub
+      </a>
+      <a
+        href="https://www.linkedin.com/in/monicamendesmontanha/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        LikedIn
+      </a>
+      <a
+        href="https://twitter.com/monica_mendes_m"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Twitter
+      </a>
+    </div>
+  </>
 )
