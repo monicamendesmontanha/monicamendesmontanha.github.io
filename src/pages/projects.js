4b5868b01e07c1b854453e9faa9233c981589050
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-17 18:51:44 +1000

    Template style gradient projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,60 +13,120 @@ export default () => (
     <h3>PROJECTS</h3>
     <div className="container">
       <div className="item">
-        <div className="summary">
-          <div>
-            MY NEWS
+        <div className="descriptionItem">
+          <div className="projectName">MY NEWS</div>
+          <div className="summary">
+            <li>
+              Listen and read the news around the world in an accessible way
+            </li>
+            <li>Play, Pause, Resume and Stop any time the speaking content.</li>
+            <li>
+              Read without distractions, the content is showed without
+              advertising.
+            </li>
+            <li>Click and go to the original article page.</li>
           </div>
-          <div>
-          <li>Listen and read the news around the world in an accessible way</li>
-          <li>Play, Pause, Resume and Stop any time the speaking content.</li>
-          <li>Read without distractions, the content is showed without advertising.</li>
-          <li>Click and go to the original article page.</li>
+          {/* <div className="techUsed">
+            <div className="tag">React</div>
+            <div className="tag">Node.js</div>
+            <div className="tag">Express</div>
+            <div className="tag">SASS</div>
+          </div> */}
+        </div>
+        <div className="display">
+          <img
+            className="iphone"
+            src={MyNewsIphone}
+            alt="My News Iphone"
+            height="240"
+            width="140"
+          />
+          <img
+            className="ipad"
+            src={MyNewsIpad}
+            alt="My News Ipad"
+            height="300"
+            width="240"
+          />
+        </div>
+        <div className="techUsed">
+            <div className="tag">React</div>
+            <div className="tag">Node.js</div>
+            <div className="tag">Express</div>
+            <div className="tag">SASS</div>
           </div>
-          <div className="techUsed">React, Node.js/Express and SASS</div>
-        </div>
-        <div className="monitor">
-        <img src={MyNewsIphone} alt="My News Iphone" height="240" width="140" />
-        <img src={MyNewsIpad} alt="My News Ipad" height="300" width="240" />
-        </div>
       </div>
 
       <div className="item">
-      <div className="summary">
-          <div>
-            TRIP WALLET
+        <div className="descriptionItem">
+          <div className="projectName">TRIP WALLET</div>
+          <div className="summary">
+            <li>Keep track of your expenses while travelling.</li>
+            <li>Automatically conversion the rates.</li>
+            <li>Enjoy travelling and saving money at the same time.</li>
+            <li>
+              Store and visualise all your expenses organized by category.
+            </li>
           </div>
-          <div>
-          <li>Keep track of your expenses while travelling.</li>
-          <li>Automatically conversion the rates.</li>
-          <li>Enjoy travelling and saving money at the same time.</li>
-          <li>Store and visualise all your expenses organized by category.</li>
+          {/* <div className="techUsed">
+            <div className="tag">Ruby on Rails</div>
+            <div className="tag">Postgress</div>
+            <div className="tag">CSS</div>
+          </div> */}
+        </div>
+        <div className="display">
+          <img
+            className="iphone"
+            src={TripWalletIphone}
+            alt="Trip Wallet Iphone"
+            height="240"
+            width="140"
+          />
+          <img
+            className="ipad"
+            src={TripWalletIpad}
+            alt="Trip Wallet Ipad"
+            height="300"
+            width="240"
+          />
+        </div>
+        <div className="techUsed">
+            <div className="tag">Ruby on Rails</div>
+            <div className="tag">Postgress</div>
+            <div className="tag">CSS</div>
           </div>
-          <div className="techUsed">Ruby on Rails, Postgress and CSS</div>
-        </div>
-        <div className="monitor">
-        <img src={TripWalletIphone} alt="Trip Wallet Iphone" height="240" width="140" />
-        <img src={TripWalletIpad} alt="Trip Wallet Ipad" height="300" width="240" />
-        </div>
       </div>
 
-
       <div className="item">
-      <div className="summary">
-          <div>
-          TIC TAC TOE
+        <div className="descriptionItem">
+          <div className="projectName">TIC TAC TOE</div>
+          <div className="summary">
+            <li>Keep track of your expenses while travelling.</li>
+            <li>Automatically conversion the rates.</li>
+            <li>Enjoy travelling and saving money at the same time.</li>
+            <li>
+              Store and visualise all your expenses organized by category.
+            </li>
           </div>
-          <div>
-          <li>Keep track of your expenses while travelling.</li>
-          <li>Automatically conversion the rates.</li>
-          <li>Enjoy travelling and saving money at the same time.</li>
-          <li>Store and visualise all your expenses organized by category.</li>
+          {/* <div className="techUsed">
+            <div className="tag">Javascript</div>
+            <div className="tag">HTML</div>
+            <div className="tag">CSS</div>
+          </div> */}
+        </div>
+        <div className="display">
+          <img
+            src={TicTacToeMonitor}
+            alt="Tic Tac Toe Monitor"
+            height="240"
+            width="340"
+          />
+        </div>
+        <div className="techUsed">
+            <div className="tag">Javascript</div>
+            <div className="tag">HTML</div>
+            <div className="tag">CSS</div>
           </div>
-          <div className="techUsed">Javascript, HTML and CSS</div>
-        </div>
-        <div className="monitor">
-        <img src={TicTacToeMonitor} alt="Tic Tac Toe Monitor" height="240" width="340" />
-        </div>
       </div>
     </div>
   </>
