dffc26a4151a850071db85dccf60cc60de265aba
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-18 12:58:42 +1000

    Add initial style to skills/ interests page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,15 +1,16 @@
 import React from "react"
+import "./scss/skills.scss"
 import Menu from "./components/Menu"
 
 export default () => (
   <>
     <Menu />
     <h3>SKILLS</h3>
-    <p>
+    <p className="skills">
       Javascript     React    Ruby    Ruby on Rails   Express
       Node.js    JQuery    Graphql   HTML 5    CSS 3    SASS
     </p>
     <h3>INTERESTS</h3>
-    <p>Accessibility   Planning  Productivity   Mentoring</p>
+    <p className="interests">Accessibility   Planning  Productivity   Mentoring</p>
   </>
 )
