682f9c349fdcc5b5e2d029bd60aec44f3e041346
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-18 10:26:12 +1000

    Add footer to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./scss/contact.scss"
 import Menu from "./components/Menu"
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMapMarkerAlt, faEnvelope} from "@fortawesome/free-solid-svg-icons";
@@ -12,19 +13,23 @@ export default () => (
     <Menu />
     <h3>CONTACT</h3>
     <p>If you have any questions, please don't hesitate to contact using form below...</p>
+
     <form>
       <input placeholder="Name"></input>
       <input placeholder="Email"></input>
       <input placeholder="Message"></input>
       <button type="submit">Send</button>
     </form>
-    <div>
-      <div>
-        <FontAwesomeIcon icon={faMapMarkerAlt} />
+
+
+    <div className="footer">
+      <div className="cityFooter">
+        <FontAwesomeIcon icon={faMapMarkerAlt} className="cityIcon" />
         Sydney, NSW
       </div>
-      <div>
-      <FontAwesomeIcon icon={faEnvelope} />
+      <div>•</div>
+      <div className="emailFooter">
+      <FontAwesomeIcon icon={faEnvelope} className="emailIcon" />
       monicamendesmontanha@gmail.com</div>
     </div>
   </>
