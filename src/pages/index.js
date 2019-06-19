c7cd5c5a6d758b5f36cf26fd8f522d70ddc16acd
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-19 18:16:04 +1000

    Add footer on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Menu from "./components/Menu"
+import Footer from "./components/Footer"
 import "./scss/index.scss"
 
 export default () => (
@@ -8,6 +9,7 @@ export default () => (
     <div className="main">
       <h1>Monica Mendes Montanha</h1>
       <h2>Junior Software Engineer</h2>
+      <Footer />
     </div>
   </>
 )
