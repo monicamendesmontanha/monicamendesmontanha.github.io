fbc957d824aa55b441f7add70adeaae770cd3c4d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-21 10:59:53 +1000

    Change style on initial page, create animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,36 @@ export default () => (
   <>
     <Menu />
     <div className="main">
+      <p>
+        "I am always looking for the next step, a different world to go into,
+        areas where I have not been before."
+      </p>
       <h1>Monica Mendes Montanha</h1>
-      <h2>Junior Software Engineer</h2>
+      {/* <h1>
+        <span class="glitch" data-text="Monica">
+        Monica{" "}
+        </span>
+        <span class="glitch" data-text="Mendes">
+        Mendes
+        </span>{" "}
+        <span class="glitch" data-text="Montanha">
+        Montanha
+        </span>
+      </h1> */}
+      {/* <h2>Junior Software Engineer</h2> */}
+
+      <h2>
+        <span class="glitch" data-text="Junior">
+          Junior{" "}
+        </span>
+        <span class="glitch" data-text="Software">
+          Software
+        </span>{" "}
+        <span class="glitch" data-text="Engineer">
+          Engineer
+        </span>
+      </h2>
+
       <Footer />
     </div>
   </>
