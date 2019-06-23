4c98e6377b9183510b9599f29441c9d2ba7f4ead
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-23 19:36:48 +1000

    Change the content of the text on about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,35 +20,51 @@ export default () => (
         </p>
 
         <p>
-          When I set my mind to something I work very hard to achieve it.
-          Whether it be building a career as a physiotherapist in Brazil, coming
+          When I set my mind to something I work very hard to achieve it:
+          whether it be building a career as a physiotherapist in Brazil, coming
           to Sydney to learn English or my current focus of reskilling as a
           software engineer.
         </p>
 
         <p>
-          I was a physiotherapist for 7 years, I always have worked helping the
+          Working as a physiotherapist for 7 years, I focused on helping the
           development of children with impairments. I have worked in different
           scenarios like Intensive Care Unit (ICU) at hospitals, hydrotherapy at
           the pool and at clinics at the gymnasium.
         </p>
 
         <p>
-          At a certain point, I realized that I had achieved my goals as a
-          physiotherapist and I decided for a career-change trying something
-          that I already admired and I didn't have the chance to work before.
+          Realising that I had achieved my goals as a physiotherapist, I decided
+          for a career-change trying something that I already admired and hadn't
+          had the chance to work as before.
         </p>
 
         <p>
-          I have embedded myself in the Sydney tech industry by attending <Link to="/techCommunity/">meetups</Link>, enrolling in the <a target="_blank" rel="noopener noreferrer" href="https://levelup.thoughtworks.com/about-levelup" >LevelUp</a> program at ThoughtWorks and studying
-          the <a target="_blank" rel="noopener noreferrer" href="https://generalassemb.ly/education/software-engineering-immersive/sydney" >Software Engineering Immersive</a> course at General Assembly.
+          I have embedded myself in the Sydney tech industry by attending{" "}
+          <Link to="/techCommunity/">meetups</Link>, enrolling in the{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://levelup.thoughtworks.com/about-levelup"
+          >
+            LevelUp
+          </a>{" "}
+          program at ThoughtWorks and studying the{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://generalassemb.ly/education/software-engineering-immersive/sydney"
+          >
+            Software Engineering Immersive
+          </a>{" "}
+          course at General Assembly.
         </p>
 
         <p>
           I love the ever-changing nature of this industry and that{" "}
           <span>
-            {" "}
-            I have chosen a career path where I will always be learning.{" "}
+            I am thrilled to have chosen a career path where I will always be
+            learning.
           </span>{" "}
           I also love the diversity of the tech community - I feel like I have
           found my people!
