ca4a1ee9a5563a555c6b262df1f3e0786e062edf
Monica Mendes <monicamendesmontanha@gmail.com>
2021-01-31 21:03:38 +1100

    feat: update content on tech community page

--- src/pages/techCommunity.js
+++ src/pages/techCommunity.js
@@ -54,7 +54,7 @@ export default () => (
     <h3>TECH COMMUNITY</h3>
     <p className="techCommunitySentence">
       Here are a few of the events, meetups and programs I've been involved in
-      over the past 2 years in the Sydney tech community.
+      over the past years in the Sydney tech community.
     </p>
     <div className="techCommunity">
 
